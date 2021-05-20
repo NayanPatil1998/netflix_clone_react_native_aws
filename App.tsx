0334b2fbdd6ccc6ceca099da5fd4622415db5209
Nayan Patil <patil.nayan04@gmail.com>
2021-05-20 15:29:44 +0530

    setup amplify and Auth

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import Amplify from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react-native";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import config from "./src/aws-exports";
 
-export default function App() {
+Amplify.configure(config);
+
+const App = () => {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -20,4 +25,6 @@ export default function App() {
       </SafeAreaProvider>
     );
   }
-}
+};
+
+export default withAuthenticator(App);
